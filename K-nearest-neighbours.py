901014abbe5a720909f6af963c6f19f09df43800
Rushabh Mehta <rushabh123@gmail.com>
2018-09-22 14:36:34 +0530

    Calculating error for the simple KNN model

--- K-nearest-neighbours.py
+++ K-nearest-neighbours.py
@@ -7,6 +7,7 @@ Created on Sat Sep 22 10:52:36 2018
 
 import os
 import pandas as pd
+import numpy as np
 
 #Setting the path
 os.getcwd()
@@ -23,19 +24,27 @@ dc_listings1["distance"]=abs(dc_listings["accommodates"]-3)
 list(dc_listings1["distance"].value_counts())
 
 #Cleaning the price column
-dc_listings["price"]
 dc_listings["price"] = dc_listings["price"].str.replace(",","")
 dc_listings["price"] = dc_listings["price"].str.replace("$","").astype("float")
+dc_listings1["price"] = dc_listings1["price"].str.replace(",","")
+dc_listings1["price"] = dc_listings1["price"].str.replace("$","").astype("float")
+
 dc_listings["price"].describe()
 
 #Finding the mean prices of all the houses with 3 rooms
 mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()
 mean
 
-#Function for the above code
-def get_mean(number_of_rooms):
+#Function that finds the absolute distance for accomodates and finds the predicted price
+def get_mean(number_of_rooms,k,dc_listings1):
     dc_listings1["distance"]=abs(dc_listings["accommodates"]-number_of_rooms)
-    mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()
-    return mean
+    dc_listings1 = dc_listings1.loc[np.random.permutation(len(dc_listings1))]
+    mean = dc_listings1[dc_listings1["distance"]==0]["price"][:k].mean()
+    error = (dc_listings1["price"]-mean)**2
+    return mean,error.sum()**0.5
 
-get_mean()
+print(get_mean(3,5,dc_listings))
+
+#def knn(k,dc_listings1,num_rooms,num_bathrooms):
+   # dc_listings1[]=dc_listings1["accommodates"]-num_rooms
+    
