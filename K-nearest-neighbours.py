25064a9447a0be25278aaa94adf4de0351a1667d
Rushabh Mehta <rushabh123@gmail.com>
2018-09-22 15:41:01 +0530

    Added a function that calculates the euclidean distance of three variables for finding the KNN

--- K-nearest-neighbours.py
+++ K-nearest-neighbours.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Sep 22 10:52:36 2018
 
-@author: rusha
+@author: rushabh
 """
 
 import os
@@ -32,19 +32,26 @@ dc_listings1["price"] = dc_listings1["price"].str.replace("$","").astype("float"
 dc_listings["price"].describe()
 
 #Finding the mean prices of all the houses with 3 rooms
-mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()
-mean
+mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()  
 
 #Function that finds the absolute distance for accomodates and finds the predicted price
-def get_mean(number_of_rooms,k,dc_listings1):
-    dc_listings1["distance"]=abs(dc_listings["accommodates"]-number_of_rooms)
-    dc_listings1 = dc_listings1.loc[np.random.permutation(len(dc_listings1))]
-    mean = dc_listings1[dc_listings1["distance"]==0]["price"][:k].mean()
-    error = (dc_listings1["price"]-mean)**2
-    return mean,error.sum()**0.5
+#def get_mean(number_of_rooms,k,dc_listings1):
+#    np.random.seed(6)
+#    dc_listings1["distance"]=abs(dc_listings["bedrooms"]-number_of_rooms)
+#    dc_listings1 = dc_listings1.loc[np.random.permutation(len(dc_listings1))]
+#    mean = dc_listings1[dc_listings1["distance"]==0]["price"][:k].mean()
+#    error = (dc_listings1[dc_listings1["distance"]==0]["price"]-mean)**2
+#    return mean,(error.sum()/len(error))**0.5
 
-print(get_mean(3,5,dc_listings))
 
-#def knn(k,dc_listings1,num_rooms,num_bathrooms):
-   # dc_listings1[]=dc_listings1["accommodates"]-num_rooms
+def knn(k,dc_listings1,num_rooms,num_bathrooms,num_bedrooms):
+    dc_listings1["distance_accomodates"]=pd.Series(np.square(np.array(dc_listings1["accommodates"]-num_rooms)))
+    dc_listings1["distance_bathrooms"]=pd.Series(np.square(np.array(dc_listings1["bathrooms"]-num_bathrooms)))
+    dc_listings1["distance_bedrooms"]=pd.Series(np.square(np.array(dc_listings1["bedrooms"]-num_bedrooms)))
+    dc_listings1["total_distance"] = (dc_listings1["distance_accomodates"]+dc_listings1["distance_bathrooms"]+dc_listings1["distance_bedrooms"])**0.5
+    dc_listings1 = dc_listings1.sort_values(by=["total_distance"])
+    mean = dc_listings1["price"][:k].mean()
+    return mean
+    
+print(knn(5,dc_listings1,10,3,2))
     
