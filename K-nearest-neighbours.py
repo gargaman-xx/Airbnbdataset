76e16f200d89eab30f9f0f7b2e583fa1ac99fb40
Rushabh Mehta <rushabh123@gmail.com>
2018-09-22 12:37:51 +0530

    Updated python file with a very simple KNN

--- K-nearest-neighbours.py
+++ K-nearest-neighbours.py
@@ -7,9 +7,35 @@ Created on Sat Sep 22 10:52:36 2018
 
 import os
 import pandas as pd
+
+#Setting the path
 os.getcwd()
 os.chdir("C:/Users/rusha/Desktop/Airbnb")
-df = pd.read_csv("dc_airbnb.csv")
-df
-df.info()
 
+#Load the csv and create a copy
+dc_listings = pd.read_csv("dc_airbnb.csv")
+list(dc_listings)
+dc_listings1= dc_listings.copy()
+
+
+#Finding the distance of my house with 3 rooms compared to other houses
+dc_listings1["distance"]=abs(dc_listings["accommodates"]-3)
+list(dc_listings1["distance"].value_counts())
+
+#Cleaning the price column
+dc_listings["price"]
+dc_listings["price"] = dc_listings["price"].str.replace(",","")
+dc_listings["price"] = dc_listings["price"].str.replace("$","").astype("float")
+dc_listings["price"].describe()
+
+#Finding the mean prices of all the houses with 3 rooms
+mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()
+mean
+
+#Function for the above code
+def get_mean(number_of_rooms):
+    dc_listings1["distance"]=abs(dc_listings["accommodates"]-number_of_rooms)
+    mean = dc_listings[dc_listings1["distance"]==0]["price"].mean()
+    return mean
+
+get_mean()
